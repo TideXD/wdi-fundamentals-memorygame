729ca20c2677d34b4124fcbfd82fd08b0d7824ce
TideXD <monster2972@yahoo.com>
2017-06-20 07:48:18 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var cards = ["queen", "queen", "king", "king"];
+cardsInPlay = cards.push('cardOne', 'cardTwo');
+var cardOne = "queen"
+var cardTwo = "king"
+console.log("User flipped queen");
+console.log("User flipped king");
+if (cardsInPlay === '2')
+if (cardsInPlay === [0]);
+alert("You found a match!");
+alert("Sorry, try again.");
