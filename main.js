60b13a36bddf8a7bed4be0cdb7b09b83ed00e782
TideXD <monster2972@yahoo.com>
2017-06-21 09:18:00 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+var cards = {
+
+	rank: "queen", "queen", "king", "king",
+	suit: "hearts", "diamonds", "hearts", "diamonds",
+	cardImage: "images/queen-of-hearts.png", "images/queen-of-diamonds.png", "images/king-of-hearts.png", "images/king-of-diamonds.png",
+};
+var cardsInPlay = cards.push('cardOne', 'cardTwo');
+
+var checkForMatch = (flipCard);
+
+if (cardsInPlay[0] === cardsInPlay[1]) {
+      console.log("You found a match!");
+  } else {
+      console.log("Sorry, try again.");
+  }
+
+var flipCard = (cards);
+console.log("User flipped " + "cards.rank");
+
+flipCard = function() {
+console.log("flipped over");
+
+};
+flipCard();
+
+var createBoard = ()
+for (var createBoard = 0; i < createBoard.length; createBoard++) {
+
+var createBoard = document.createElement('img');
+createBoard.setAttribute('createBoard', 'attributeValue');
+var createBoard = document.createBoard('data-id');
+var createBoard = document.getElementById('game-board');
+  }
+
+var cardOne = cards [0]; 
+cardsInPlay.push(cardOne);
+console.log("User flipped " + cardOne);
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo);
+
+alert("You found a match!");
+alert("Sorry, try again.");
